8c907dc4371daa8d249df1ccfe48ebb59258c28e
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-02-19 07:59:02 +0300

    Eigen added

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <Eigen/Dense>
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
 #include <windows.h>
 #include <cmath>
@@ -29,6 +32,15 @@ LRESULT CALLBACK on_event(HWND handle, UINT message, WPARAM wParam, LPARAM lPara
 
 int main()
 {
+    // Eigen test
+    Eigen::MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+    std::cout << m << std::endl;
+
+    // SFML test
     HINSTANCE instance = GetModuleHandle(NULL);
 
     WNDCLASS windowClass;
