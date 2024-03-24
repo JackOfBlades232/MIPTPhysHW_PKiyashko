4891f4c1f0f80d3112283c8d1b50049ba637b893
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-03-24 19:45:11 +0300

    exp1: gas pressure, just spins

--- draw.hpp
+++ draw.hpp
@@ -8,6 +8,25 @@
 namespace draw
 {
 
+class Box : public sf::Drawable {
+    box2d_t m_box{};
+
+public:
+    Box(box2d_t box) : m_box(box) {}
+    Box(double xmin, double ymin, double xmax, double ymax)
+        : m_box(box2d_t{vec2d_t{xmin, ymin}, vec2d_t{xmax, ymax}}) {}
+
+    virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const override { 
+        sf::Vertex nodes[5];
+        nodes[0].position = sf::Vector2f(m_box.min.x, m_box.min.y);
+        nodes[1].position = sf::Vector2f(m_box.min.x, m_box.max.y);
+        nodes[2].position = sf::Vector2f(m_box.max.x, m_box.max.y);
+        nodes[3].position = sf::Vector2f(m_box.max.x, m_box.min.y);
+        nodes[4].position = nodes[0].position;
+        target.draw(nodes, 5, sf::LinesStrip, states);
+    }
+};
+
 template <size_t t_num_points>
 class CircularLine : public sf::Drawable {
     sf::CircleShape m_points[t_num_points];
--- phys.hpp
+++ phys.hpp
@@ -2,6 +2,7 @@
 #include "utils.hpp"
 #include "span.hpp"
 
+#include <cmath>
 #include <vector>
 
 namespace phys
@@ -19,24 +20,26 @@ void verlet_apply_world_constraints(Span<VerletMassPoint2d> points);
 //                   and make use of SOA for all attribute updates
 class VerletMassPoint2d {
     friend void verlet_apply_world_constraints(Span<VerletMassPoint2d>);
-    friend class Verlet2PointsConstraint;
+    friend class Verlet2PointsConstraint2d;
+    friend class VerletGasPressureConstraint2d;
 
     vec2d_t m_pos      {};
     vec2d_t m_prev_pos {};
     vec2d_t m_force    {};
 
-    double m_mass = 0.0;
+    double m_mass       = 0.0;
+    double m_damp_factor = 0.0;
 
 public:
     VerletMassPoint2d() = default;
     // @TODO(PKiyashko): add init velocity
-    explicit VerletMassPoint2d(vec2d_t init_pos, double mass) 
-        : m_mass(mass), m_pos(init_pos), m_prev_pos(init_pos) {};
+    explicit VerletMassPoint2d(vec2d_t init_pos, double mass, double damp_factor)
+        : m_mass(mass), m_damp_factor(damp_factor), m_pos(init_pos), m_prev_pos(init_pos) {};
 
     void Integrate(double dt) {
         vec2d_t acc = m_force/m_mass;
         vec2d_t cur_pos = m_pos;
-        m_pos += cur_pos - m_prev_pos + acc*dt*dt;
+        m_pos += (cur_pos - m_prev_pos) * m_damp_factor + acc*dt*dt;
         m_prev_pos = cur_pos;
     }
 
@@ -45,16 +48,16 @@ public:
     vec2d_t GetPosition() const { return m_pos; }
 };
 
-class Verlet2PointsConstraint {
+class Verlet2PointsConstraint2d {
     double m_rest_length      = 0.0;
     double m_relaxation_coeff = 1.0;
 
 public:
-    Verlet2PointsConstraint() = default;
-    explicit Verlet2PointsConstraint(double rest_length, double relaxation_coeff)
+    Verlet2PointsConstraint2d() = default;
+    explicit Verlet2PointsConstraint2d(double rest_length, double relaxation_coeff)
         : m_rest_length(rest_length), m_relaxation_coeff(relaxation_coeff) {}
 
-    // @SPEED(PKiyashko): this is not optimized at all (verbose & w/ sqrt), and could be if it will be needed.
+    // @SPEED(PKiyashko): this is not optimized at all (verbose & w/ 2xsqrt), and could be if it will be needed.
     void Apply(VerletMassPoint2d &p1, VerletMassPoint2d &p2) {
         vec2d_t offset = p1.m_pos - p2.m_pos;
         double length  = vlen(offset);
@@ -65,6 +68,48 @@ public:
         // Switch because heavier point gets offset less
         p1.m_pos -= shift_base * (1.0 - first_point_weight);
         p2.m_pos += shift_base * first_point_weight;
+    }
+};
+
+class VerletGasPressureConstraint2d {
+    double m_rest_area = 0.0;
+    double m_relaxation_coeff = 1.0;
+
+public:
+    VerletGasPressureConstraint2d() = default;
+    explicit VerletGasPressureConstraint2d(double rest_area, double relaxation_coeff)
+        : m_rest_area(rest_area), m_relaxation_coeff(relaxation_coeff) {}
+
+    // @FEATURE(PKiyashko): this could be remade to take an interface/be templated to
+    //                      be able to take any container, even not contiguous.
+    // @SPEED(PKiyashko):   This is, again, not necessarily optimal.
+    void Apply(Span<VerletMassPoint2d> points) {
+        // Area by shoelace formula (sum of oriented trapezoid areas)
+        double area = 0.0;
+        vec2d_t mass_center{};
+        for (size_t i = 0; i < points.Size(); ++i) {
+            size_t ip1 = i == points.Size()-1 ? 0 : i+1;
+            area += (points[i].m_pos.y + points[ip1].m_pos.y) * (points[i].m_pos.x - points[ip1].m_pos.x);
+            mass_center += points[i].m_pos;
+        }
+        area *= 0.5;
+        mass_center /= (double)points.Size();
+
+        // @SPEED(PKiyashko): will storing inv_rest_area be faster?
+        double area_coeff = area / m_rest_area;
+        double linear_offset_coeff = sqrt(area_coeff) - 1.0;
+
+        // @TODO(PKiyashko): This does not take masses into account.
+        //                   I don't quite understand how to factor in masses properly,
+        //                   meaning, that it is unclear how to distribute constraints to points w/
+        //                   different masses. This may be useful to return to.
+        for (VerletMassPoint2d &point : points) {
+            vec2d_t radial_vector = point.m_pos - mass_center;
+            // Negated because we are counteracting the offset
+            double full_offset = -vlen(radial_vector) * linear_offset_coeff * m_relaxation_coeff;
+
+            point.m_pos += vnormalize(radial_vector) * full_offset;
+        }
     }
 };
 
